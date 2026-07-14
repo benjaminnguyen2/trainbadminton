6bc0ee1a99e1069e9ea6e9b62a0d296341cee353
Benjamin <bn.gofm5@gmail.com>
2026-07-14 02:28:14 -0400

    fix: updated syntax in vite.ts

--- trainbadminton.com/vite.config.ts
+++ trainbadminton.com/vite.config.ts
@@ -7,13 +7,7 @@ export default defineConfig({
 	plugins: [
 		enhancedImages(),
 		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
 			adapter: adapter()
 		})
 	]
 });
-
-
